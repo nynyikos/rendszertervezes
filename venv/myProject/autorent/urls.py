2e6c293cf47717d9c964e3f1a09360ed7672f5dc
nynyikos <ny.nyikos@gmail.com>
2024-04-25 10:39:13 +0200

    default server-kliens api kommunikáció hozzáadása

--- venv/myProject/autorent/urls.py
+++ venv/myProject/autorent/urls.py
@@ -1,8 +1,20 @@
 from . import views
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import UserViewSet, CategoryViewSet, CarViewSet, RentalViewSet, SaleViewSet
 
+
+router = DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'categories', CategoryViewSet)
+router.register(r'cars', CarViewSet)
+router.register(r'rentals', RentalViewSet)
+router.register(r'sales', SaleViewSet)
 
 urlpatterns = [
     path('',views.index,name='index'),
     path('item/',views.item,name='item'),
+    path('', include(router.urls)),
 ]
+
+
--- venv/myProject/autorent/views.py
+++ venv/myProject/autorent/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework import viewsets
+from .models import user, category, car, rental, sale
+from .serializers import UserSerializer, CategorySerializer, CarSerializer, RentalSerializer, SaleSerializer
 
 
 # Create your views here.
@@ -8,3 +11,24 @@ def index(request):
 
 def item(request):
     return HttpResponse('<h1>There will be a car here<h1>')
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = user.objects.all()
+    serializer_class = UserSerializer
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = category.objects.all()
+    serializer_class = CategorySerializer
+
+class CarViewSet(viewsets.ModelViewSet):
+    queryset = car.objects.all()
+    serializer_class = CarSerializer
+
+class RentalViewSet(viewsets.ModelViewSet):
+    queryset = rental.objects.all()
+    serializer_class = RentalSerializer
+
+class SaleViewSet(viewsets.ModelViewSet):
+    queryset = sale.objects.all()
+    serializer_class = SaleSerializer
