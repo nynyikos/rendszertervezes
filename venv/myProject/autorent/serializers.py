81f386ff7e2a2197072e8ec22fe157838ad96d06
nynyikos <ny.nyikos@gmail.com>
2024-05-02 17:34:57 +0200

    pip freeze

--- venv/myProject/autorent/serializers.py
+++ venv/myProject/autorent/serializers.py
@@ -1,7 +1,24 @@
 from rest_framework import serializers
-from .models import user
+from .models import user, category, car, rental, sale
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = user
-        fields = ['id', 'username', 'name', 'password']
+        fields = '__all__'
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = category
+        fields = '__all__'
+
+
+
+class RentalSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = rental
+        fields = '__all__'
+
+class SaleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = sale
+        fields = '__all__'
